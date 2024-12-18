2543d4487573b4b03268f9afda4cdb2854e973fa
shrik121 <shreyansh.kumar32@outlook.com>
2024-12-18 23:15:45 +0530

    updated

--- hotelservice/src/main/java/com/hotelservice/hotelservice/servicesImpl/HotelServiceImpl.java
+++ hotelservice/src/main/java/com/hotelservice/hotelservice/servicesImpl/HotelServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class HotelServiceImpl implements HotelService {
@@ -18,6 +19,7 @@ public class HotelServiceImpl implements HotelService {
 
     @Override
     public Hotel createHotel(Hotel hotel) {
+        hotel.setHotelId(UUID.randomUUID().toString());
         return hotelRepo.save(hotel);
     }
 
