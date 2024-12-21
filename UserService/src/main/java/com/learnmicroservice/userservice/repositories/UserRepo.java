3a423c272bcae726d4bb45468d0170da2f018ece
Shreyansh Kumar <127504065+Shrik21@users.noreply.github.com>
2024-12-21 20:01:59 +0530

    Delete UserService/src/main/java/com/learnmicroservice/userservice directory

--- UserService/src/main/java/com/learnmicroservice/userservice/repositories/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.learnmicroservice.userservice.repositories;
-
-import com.learnmicroservice.userservice.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-}
